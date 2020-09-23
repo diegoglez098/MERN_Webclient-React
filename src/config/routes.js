e32e83eed6d6d2ed5599b71c068a052cec0f0748
Diego Gonzalez <diego.gonzalez.1608@gmail.com>
2020-09-23 16:36:15 +0200

    Initial commit

---
+++ src/config/routes.js
@@ -0,0 +1,31 @@
+//Layout
+
+import LayoutAdmin from "../layouts/LayoutAdmin";
+
+//Admin Pages
+
+import AdminHome from "../pages/admin";
+
+import AdminSignIn from "../pages/admin/SignIn"
+
+const routes = [
+    {
+        path: "/admin",
+        component: LayoutAdmin,
+        exact:false,
+        routes:[
+            {
+                path:"/admin",
+                component: AdminHome,
+                exact: true
+            },
+            {
+                path:"admin/login",
+                component:AdminSignIn,
+                exact: true
+            }
+        ]
+    }
+];
+
+export default routes;
---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,40 @@
+import React from "react"
+import {Route} from 'react-router-dom'
+
+import "./LayoutAdmin.scss";
+
+import {Layout} from "antd";
+//import routes from "../config/routes";
+
+export default function LayoutAdmin(props){
+
+    const {routes} = props;
+    const {Header, Content, Footer} =Layout;
+
+    return (
+      <Layout>
+          <h2>Menu Sider Admin</h2>
+          <Layout>
+              <Header>Cabecera...</Header>
+              <Content>
+                 <LoadRouters routes ={routes} />
+              </Content>
+              <Footer>
+                  Diego Gonzalez 2020
+              </Footer>
+          </Layout>
+      </Layout>  
+    );
+}
+
+function LoadRouters ({routes}) {
+    
+    return routes.map((route, index) => (
+        <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        component = {route.component}
+        />
+    ))
+}
